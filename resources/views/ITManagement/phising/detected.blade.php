0aa16d185bb23d974caab28097027badc4a54412
Fitrah Pradana <pradanafitrah45@gmail.com>
2024-02-23 17:18:01 +0700

    v16.1

--- resources/views/ITManagement/phising/detected.blade.php
+++ resources/views/ITManagement/phising/detected.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title>Contoh Gambar di HTML</title>
 </head>
-<body style="background-image: url({{ asset('/') }}template_hrsm/CybAttck.jpeg);">
+<body style="background-image: url({{ asset('/') }}template_hrsm/CybAttck2.jpeg);">
   {{-- <h1>Background dengan Gmabar</h1>
   <p>Halaman ini menggunakan gambar sebagai background</p> --}}
 </body>
